b7760b0d5f12b1436e234c3f63634a6590d09916
Barlo <barlo@list.ru>
2019-07-07 19:13:21 +0400

    HW05 impl without optional

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,9 +1,12 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.service.UserService;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,31 +14,39 @@ import java.util.List;
 @Repository
 public class DataJpaMealRepository implements MealRepository {
 
+    private static final Sort SORT_DATETIME = new Sort(Sort.Direction.DESC, "dateTime");
+
     @Autowired
     private CrudMealRepository crudRepository;
+    @Autowired
+    private UserService service;
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
+            return null;
+        }
+        meal.setUser(service.get(userId));
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.findById(id, userId).orElse(null);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findAll(userId);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return crudRepository.findAllSorted(startDate, endDate, userId);
     }
 }
