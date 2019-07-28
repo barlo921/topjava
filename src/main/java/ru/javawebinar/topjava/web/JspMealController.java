f5aa933fb535977af741a1fb9c20e778cd8ea873
Barlo <barlo@list.ru>
2019-07-28 09:46:04 +0400

    HW06 not ready yet

---
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -0,0 +1,94 @@
+package ru.javawebinar.topjava.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.web.meal.MealRestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+
+@Controller
+public class JspMealController {
+
+    @Autowired
+    private MealRestController mealController;
+
+    @PostMapping("/mealForm")
+    public String addMeal(Model model, HttpServletRequest request) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+
+        if (StringUtils.isEmpty(request.getParameter("id"))) {
+            mealController.create(meal);
+        } else {
+            mealController.update(meal, getId(request));
+        }
+        return "redirect:meals";
+    }
+
+    @GetMapping("/mealForm")
+    public String mealForm(Model model, @RequestParam Meal meal) {
+        model.addAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @GetMapping("/meals")
+    public String meals(Model model, HttpServletRequest request, RedirectAttributes ra) {
+        String action = request.getParameter("action");
+        String redirect;
+
+        switch (action == null ? "all" : action) {
+            case "delete":
+                int id = getId(request);
+                mealController.delete(id);
+                redirect = "meals";
+                break;
+            case "create":
+            case "update":
+                final Meal meal = "create".equals(action) ?
+                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
+                        mealController.get(getId(request));
+                ra.addAttribute("meal", meal);
+                redirect = "redirect:mealForm";
+                break;
+            case "filter":
+                LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+                LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+                LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+                LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+                model.addAttribute("meals", mealController.getBetween(startDate, startTime, endDate, endTime));
+                redirect = "meals";
+                break;
+            case "all":
+            default:
+                model.addAttribute("meals", mealController.getAll());
+                redirect = "meals";
+                break;
+        }
+        return redirect;
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(paramId);
+    }
+
+}
